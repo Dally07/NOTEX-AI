296671159443ee4e2086b6dd199759c8fc2f8553
Dally07 <dallymaminomenjanahar@gmail.com>
2025-05-22 00:18:36 +0300

    feat(models): ajout du fonction predict_category pour l'application auto

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from .classifier import predict_category
 
 # Create your models here.
 class Note(models.Model):
     phrase = models.TextField()
     category = models.CharField(max_length=200, blank=True)
     creat_at = models.DateTimeField(auto_now_add= True)
+
+    def save(self, *args, **kwargs):
+        if not self.category and self.phrase:
+            try:
+                self.category = predict_category(self.phrase)
+            except Exception:
+                self.category = "Inconnue"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.phrase[:30]}... ({self.category})"
