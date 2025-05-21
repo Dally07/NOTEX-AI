4af4d5e20dd9a9d6cb34b8b2bf5bb91735bf4ad7
Dally07 <dallymaminomenjanahar@gmail.com>
2025-05-21 23:23:47 +0300

    fix: corriger l’affichage et l’ordonnancement dans l’admin Note

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phrase','category', 'creat_at')
+    search_fields = ('phrase', 'category')
+    list_filter = ('category', 'creat_at')
+    ordering = ('creat_at')
+    ordering = ['id'] 
