ef4f2c4a4f182f6d86b53cc98cb46188edcf8ba8
Dally07 <dallymaminomenjanahar@gmail.com>
2025-05-21 23:32:52 +0300

    chore: utilisation d'une variable d'environement pour postgreSQL config

--- NOTEX_AI/settings.py
+++ NOTEX_AI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +80,14 @@ WSGI_APPLICATION = 'NOTEX_AI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'notex_db',
-        'USER' : 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
